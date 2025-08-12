c65bcd4a18d8bceef24b6f93dc2a21fa37932729
224Maria <xmrn224@gmail.com>
2025-08-12 10:03:33 -0500

    Creación de operaciones.

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+import numpy as np
+vector1 = np.array([1,2,3])
+vector2 = np.array([4,5,6])
+suma = vector1 + vector2
+print("Suma de vectores:", suma)
+producto = vector1 * vector2
+print("Producto de vectores:", producto)
+división = vector1 / vector2
+print("División de vectores:", división)
+resta = vector1 - vector2
+print ("Resta de vectores:", resta)
+potencia = vector1 ** 2
+print("Potencia de vectores:", potencia)
