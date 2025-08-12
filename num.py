f1f460d120c035d81a15c3d3df3e6450ef512292
224Maria <xmrn224@gmail.com>
2025-08-12 09:32:55 -0500

    Subir el archivo
    Ejemplo de vectores

---
+++ num.py
@@ -0,0 +1,21 @@
+import numpy as np
+vector = np.aray ([1, 2, 3, 4, 5])
+print(vector)
+"""Los vectores no son como las listas, no tienen un método end() 
+para agregar elementos    "vectores" Unknown word.
+o tienen un método pop() para eliminar elementos, pero sí tienen
+método reshape() para cambiar su forma, adicionalmente despues 
+creado no se puede cambiar el tamaño del vector"""  "tienen" Unknown word.
+vector1 = np.zeros(5)
+print(vector2)
+vector2 = np.ones(5)
+print(vector2)
+vector3 = np.arange(1,10)
+print("rango",vector3)
+vector4 = np.linspace(1,10,5)
+print("linspace",vector4)
+vector5 = np.random.rand(10)
+print("random",vector5)
+vector6 = np.random.randint(1,10,10)
+print("randint",vector6)
+
